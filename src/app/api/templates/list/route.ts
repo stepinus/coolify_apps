81760a87dc180d506dd65490632615ba6c53e1f6
iurii stepin <y.stepin@x5.ru>
2024-11-13 05:29:52 +0300

    fix

--- src/app/api/templates/list/route.ts
+++ src/app/api/templates/list/route.ts
@@ -6,27 +6,10 @@ export async function GET() {
   let logs = '';
 
   try {
-    const templatesDir = '/data/coolify/templates';
+    const templatesDir = 'templates';
     
-    // Log the directory path
-    logs += `Checking directory: ${templatesDir}\n`;
-    
-    // Log the current directory contents
-    const currentDir = process.cwd();
-    try {
-      const currentDirContents = await fs.readdir(currentDir);
-      logs += `Current directory (${currentDir}) contents: ${currentDirContents}\n`;
-      const upperdir = await fs.readdir('../currentDir');
-      logs += `upperDir contents: ${upperdir}\n`;      
-      // Log details of each item in the current directory
-      // for (const item of currentDirContents) {
-      //   const itemPath = path.join(currentDir, item);
-      //   const itemStats = await fs.stat(itemPath);
-      //   logs += `Item: ${item}, Type: ${itemStats.isDirectory() ? 'Directory' : 'File'}\n`;
-      // }
-    } catch (error) {
-      logs += `Error reading current directory (${currentDir}) contents: ${error}\n`;
-    }
+    // Log the directory path    
+
     
     // Check if directory exists, if not return empty array
     try {
@@ -40,12 +23,6 @@ export async function GET() {
     const templates = await fs.readdir(templatesDir);
     logs += `Templates found: ${templates}\n`;
     
-    // Log details of each item in the templates directory
-    for (const template of templates) {
-      const templatePath = path.join(templatesDir, template);
-      const templateStats = await fs.stat(templatePath);
-      logs += `Template: ${template}, Type: ${templateStats.isDirectory() ? 'Directory' : 'File'}\n`;
-    }
 
     // Check if directory is empty
     if (templates.length === 0) {
